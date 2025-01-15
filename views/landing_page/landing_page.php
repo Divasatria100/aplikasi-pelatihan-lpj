a63699998954e75baee6de14acfdd68fb961b1d2
Diva Satria <divasatria700@gmail.com>
2025-01-15 21:28:26 +0700

    Membuat landing_page

---
+++ views/landing_page/landing_page.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajukan</title>
+    <link rel="stylesheet" href="landing_page.css">
+    <script src="landing_page.js"></script>
+</head>
+<body>
+    <nav>
+        <div class="wrapper">
+            <div class="logo">
+                <a href='#'><img src='/assets/images/Logo_Ajukan.png'></a></div>
+            <div class="menu">
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#manager_project">Manager Project</a></li>
+                    <li><a href="#team_members">Team Members</a></li>
+                    <li><a href="/views/auth/login.php" class="tbl-biru">Login</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="banner">
+        <section id="home">
+        </section>
+    </div>
+        <div class="wrapper" style="margin-top: 100px;">
+            <!-- untuk courses -->
+            <section id="courses">
+                <div class="kolom">
+                    <p class="deskripsi">Aplikasi Pengajuan Usulan Pelatihan dan LPJ</p>
+                    <h2>Apa itu Ajukan?</h2>
+                    <h3 style="color: #364f6b;"><strong>Ajukan</strong> adalah aplikasi berbasis website yang dirancang untuk memudahkan proses pengajuan usulan pelatihan dan laporan pertanggungjawaban (LPJ) di lingkungan organisasi. Aplikasi ini menyediakan platform yang terintegrasi untuk mengelola pengajuan dengan lebih cepat, efisien, dan transparan. Melalui Ajukan, karyawan dapat mengajukan pelatihan sesuai kebutuhan pengembangan kompetensi mereka, melampirkan dokumen pendukung, serta melacak status pengajuan secara real-time. Selain itu, laporan pertanggungjawaban setelah pelatihan dapat disusun dan diajukan langsung melalui aplikasi. Bagi manajer, Ajukan memberikan kemudahan dalam meninjau, menyetujui, atau menolak pengajuan, serta memberikan umpan balik dengan transparansi penuh.</h3>
+                </div>
+            </section>
+    
+            <!-- untuk manpro -->
+            <section id="manager_project">
+                <div class="tengah">
+                    <div class="kolom">
+                        <h2>Manager Project</h2>
+                        <p>PBL-TRPL111</p>
+                    </div>
+    
+                    <div class="container">
+                        <div class="card">
+                            <div class="img">
+                                <img src="/assets/images/manpro.jpg" alt="">
+                            </div>
+                            <div class="content">
+                                <h3 style="font-size: 27px;">Banu Failasuf, S.Tr.</h3>
+                                <p>NIDN/NIK : 215211</p>
+                            </div>
+                        </div>
+                </div>
+            </section>
+    
+            <!-- untuk partners -->
+            <section id="team_members">
+                <div class="tengah">
+                    <div class="kolom">
+                        <h2>Team Members</h2>
+                        <p>PBL-TRPL111</p>
+                    </div>
+    
+                    <div class="container">
+                        <div class="card">
+                            <div class="img">
+                                <img src="/assets/images/divasatria.jpg" alt="">
+                            </div>
+                            <div class="content">
+                                <h3 style="font-size: 20px;">Diva Satria</h3>
+                                <p>4342401072<p>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="img">
+                                <img src="/assets/images/ariparip.jpg" alt="">
+                            </div>
+                            <div class="content">
+                                <h3 style="font-size: 20px;">Muhamad Ariffadhlullah</h3>
+                                <p>4342401070<p>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="img">
+                                <img src="/assets/images/acaaca.jpg" alt="">
+                            </div>
+                            <div class="content">
+                                <h3 style="font-size: 20px;">Arshafin Alfisyahrin</h3>
+                                <p>4342401075<p>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="img">
+                                <img src="/assets/images/pajar.jpg" alt="">
+                            </div>
+                            <div class="content">
+                                <h3 style="font-size: 20px;">Fajar Mirza Hanif</h3>
+                                <p>4342401061<p>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="img">
+                                <img src="/assets/images/tia.jpg" alt="">
+                            </div>
+                            <div class="content">
+                                <h3 style="font-size: 20px;">Navita Damayanti Syarif</h3>
+                                <p>4342401064<p>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="img">
+                                <img src="/assets/images/thalita.jpg" alt="">
+                            </div>
+                            <div class="content">
+                                <h3 style="font-size: 20px;">Thalita Aurelia Marsim</h3>
+                                <p>4342401066<p>
+                            </div>
+                        </div>
+                </div>
+            </section>
+        </div>
+    
+        <div id="copyright">
+            <div class="wrapper">
+                &copy; 2025. <b>PBL-TRPL111</b> All Rights Reserved.
+            </div>
+        </div>
+</body>
+</html>
